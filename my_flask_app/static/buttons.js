87be6b5eb49136e4e444deb57dd1c197079c1f47
Kris Lee <kdlee@kriss-mbp.lan>
2023-12-13 10:32:44 -0600

    added dynamic display function

--- my_flask_app/static/buttons.js
+++ my_flask_app/static/buttons.js
@@ -4,7 +4,10 @@ document.addEventListener("DOMContentLoaded", function(){
     const stopButton = document.querySelector(".stop");
     const nextButton = document.querySelector(".next");
     const previousButton = document.querySelector(".previous");
+    const volumeUpButton = document.querySelector(".volume-up");
+    const volumeDownButton = document.querySelector(".volume-down")
     const audio = document.getElementById("audio");
+    
     let audioArray = [ //audioArray contains all the songs in an array.
         "static/audio/Bill Evans - Skating In Central Park.mp3",
         "static/audio/Frank Sinatra - Have Yourself A Merry Little Christmas.mp3",
@@ -32,3 +35,30 @@ document.addEventListener("DOMContentLoaded", function(){
         audio.play();
     })
 })
+    // Volume control
+    volumeUpButton.addEventListener("click", function(){
+        if (audio.volume < 1.0) {
+            audio.volume = Math.min(1.0, audio.volume + 0.25); // Ensure volume doesn't exceed 1.0
+        }
+    })
+
+    volumeDownButton.addEventListener("click", function(){
+        if (audio.volume > 0) {
+            audio.volume = Math.max(0, audio.volume - 0.25); // Ensure volume doesn't go below 0
+        }
+    })
+// dynamic display function
+function getCurrentSongName() {
+    switch (currentAudioIndex) {
+        case 0:
+            return "Bill Evans - Skating In Central Park";
+        case 1:
+            return "Frank Sinatra - Have Yourself A Merry Little Christmas";
+        case 2:
+            return "Michael Bublé - It's Beginning To Look A Lot Like Christmas";
+        case 3:
+            return "The Drifters - White Christmas";
+        default:
+            return "Unknown Song";
+    }
+}
